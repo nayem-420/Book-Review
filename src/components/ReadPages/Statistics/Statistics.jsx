a0b8e44d945c381cb55d3b070d4f096a98d87e70
nayem181 <nayemimranhasan@gmail.com>
2024-05-28 20:01:06 +0600

    added a barchart in statistics

--- src/components/ReadPages/Statistics/Statistics.jsx
+++ src/components/ReadPages/Statistics/Statistics.jsx
@@ -1,9 +1,62 @@
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
 const Statistics = () => {
-    return (
-        <div>
-            <h3>this is the statistics</h3>
-        </div>
-    );
+  const data = [
+    { bookName: "The Silent Patient", totalPages: 336 },
+    { bookName: "Educated", totalPages: 352 },
+    { bookName: "Becoming", totalPages: 448 },
+    { bookName: "Where the Crawdads Sing", totalPages: 384 },
+    { bookName: "The Testaments", totalPages: 432 },
+    { bookName: "The Alchemist", totalPages: 208 },
+  ];
+
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+            ${x + width / 2}, ${y}
+            C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${
+      y + height
+    } ${x + width}, ${y + height}
+            Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+  return (
+    <div>
+      <BarChart
+        width={800}
+        height={500}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="bookName" />
+        <YAxis />
+        <Bar
+          dataKey="totalPages"
+          fill="#8884d8"
+          shape={<TriangleBar />}
+          label={{ position: "top" }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </div>
+  );
 };
 
 export default Statistics;
